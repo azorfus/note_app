26d5ea6ed7f92d08d8389acf403128e460b9065c
azorfus <72391760+azorfus@users.noreply.github.com>
2021-05-08 17:01:02 +0530

    Add files via upload

---
+++ NotesApp/edit.h
@@ -0,0 +1,40 @@
+#ifndef EDIT_H
+#define EDIT_H
+
+#include <QDialog>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QMainWindow>
+#include <QCloseEvent>
+
+class edit;
+class notes;
+class edit : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit edit(notes *parent);
+    //~edit();
+    void okpressed();
+    void cancelpress();
+    void read_vec();
+    void deletenote();
+    void replace();
+    void savefile();
+    void closeEvent(QCloseEvent *event);
+
+    notes* note;
+    bool not_okay = true;
+    QLabel *namelabel;
+    QLineEdit *namebox;
+    QTextEdit *block;
+    QPushButton *ok;
+    QPushButton *cancel;
+    QPushButton *savebutton;
+
+};
+
+#endif // EDIT_H
